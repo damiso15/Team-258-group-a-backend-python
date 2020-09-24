ceed7eb9f5e4425b7e9b0f694e3853e84ac72591
damiso15 <damiso15@yahoo.com>
2020-09-24 22:48:01 +0100

    update2

--- core/urls.py
+++ core/urls.py
@@ -36,7 +36,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1', include('employer.urls'), include('job.urls'), include('job_seeker.urls')),
+    path('api/v1', include('employer.urls')),
+    path('api/v1', include('job.urls')),
+    path('api/v1', include('job_seeker.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
